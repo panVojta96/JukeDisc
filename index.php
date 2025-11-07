be122104cbf3740a415a1c6a80a5f8ffbcc8c79f
unknown <pytlikv@ekodom.cz>
2025-11-07 12:50:01 +0100

    PrvníDen

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JukeDisc</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+</head>
+
+<body>
+    <!-- includes zatím neni, až příště :) -->
+
+    <header>
+        <div class="head-centered">
+            <a href="index.php" class="active"><img class="logo" src="images/logo_wide.png" alt=""></a>
+        </div>
+        <a href="#onas">
+            <h2>O nás</h2>
+        </a>
+        <a href="#kontakt">
+            <h2>Kontakt</h2>
+        </a>
+        <div class="head-right">
+            <a href="katalog.php">
+                <h2>Katalog</h2>
+            </a>
+            <a href="vlastni_obj.php">
+                <h2>Vlastní objednávka</h2>
+            </a>
+        </div>
+
+    </header>
+    <div class="pic">
+
+    </div>
+    <div class="nabidka">
+        <H1>Nabídka</H1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugit aut excepturi soluta, omnis dolor voluptatum. Magni, explicabo delectus reprehenderit error cumque id perspiciatis consequuntur repudiandae corporis adipisci, voluptatibus aperiam?</p>
+    </div>
+    <div class="onas" id="onas">
+        <h1>O nás</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptatibus sapiente minima accusantium eveniet sint dicta voluptates maxime fuga doloribus, nisi quam dignissimos voluptas. Delectus nemo laboriosam autem voluptatibus voluptates.</p>
+    </div>
+    <div id="kontakt" class="kontakt">
+        <h1 >Kontakt</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, vel! Voluptate quis recusandae sit quisquam culpa sunt iste error odio vero aliquam id quidem libero dolor, repellat harum iure ex?</p>
+        
+    </div>
+    <div class="footer">
+        <h3>© JukeDisc s. r. o. </h3>
+        <h3> Najdi rytmus ve svém srdci</h3>
+    </div>
+</body>
+
+</html>
